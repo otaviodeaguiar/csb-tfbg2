83eb79b16b36b85fb418bfec720beb9218a6e3f0
Code Sandbox <hello@codesandbox.io>
2021-12-22 10:10:38 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.da926a6c.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{9:function(e,t,c){"use strict";c.r(t);var n=c(1),r=c(3),o=c.n(r),s=c(0);function i(){return Object(s.jsx)("video",{playsInline:!0,autoPlay:!0,muted:!0,loop:!0,children:Object(s.jsx)("source",{src:"https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4",type:"video/mp4"})})}var a=document.getElementById("root");o.a.render(Object(s.jsx)(n.StrictMode,{children:Object(s.jsx)(i,{})}),a)}},[[9,1,2]]]);
+//# sourceMappingURL=main.da926a6c.chunk.js.map
